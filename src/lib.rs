7d0fb1094f4b7bfac8e777aed976c00167c45773
JustinRlle <justinrlle@pm.me>
2021-12-24 13:34:21 +0100

    add StrExt trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,40 @@
 use std::borrow::Cow;
 use std::fmt::Display;
-use std::str::FromStr;
+use std::iter::Map;
+use std::str::{FromStr, Lines};
 pub use itertools::Itertools;
+
+pub trait StrExt<'a> {
+    type BLinesIterator: IntoIterator<Item = &'a [u8]>;
+    fn b_lines(self) -> Self::BLinesIterator;
+    /// rust clone of https://pkg.go.dev/strings@master#Cut
+    fn cut(self, pat: char) -> Option<(Self, Self)> where Self: Sized;
+}
+
+#[derive(Debug, Clone)]
+pub struct BLines<'a>(pub(crate) Map<Lines<'a>, fn(&'a str) -> &'a [u8]>);
+
+impl<'a> Iterator for BLines<'a> {
+    type Item = &'a [u8];
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.next()
+    }
+}
+
+impl<'a> StrExt<'a> for &'a str {
+    type BLinesIterator = BLines<'a>;
+
+    fn b_lines(self) -> BLines<'a> {
+        BLines(self.lines()
+            .map(|l| l.as_bytes()))
+    }
+
+    fn cut(self, pat: char) -> Option<(Self, Self)> {
+        let idx = self.find(pat)?;
+        return Some((&self[..idx], &self[idx + 1..]));
+    }
+}
 
 pub fn b_lines(input: &str) -> impl Iterator<Item = &[u8]> {
     input.lines()
@@ -15,19 +48,17 @@ pub fn as_lossy_vec<'a>(iter: impl Iterator<Item = &'a [u8]>) -> Vec<Cow<'a, str
 
 pub fn parse_all<'a, T: FromStr>(input: &'a str) -> impl Iterator<Item=T> + 'a
     where <T as FromStr>::Err: Display {
-    input.lines()
-        .enumerate()
-        .map(|(idx, l)| {
-            l.parse::<T>()
-                .map_err(|e|
-                    format!("failed to parse {} (line {})\n  could not parse into {}: {}",
-                            l,
-                            idx,
-                            std::any::type_name::<T>(),
-                            e))
-                .unwrap()
-        })
+    input.lines().enumerate().map(|(idx, l)| {
+        l.parse::<T>().map_err(|e|
+            format!("failed to parse {} (line {})\n  could not parse into {}: {}",
+                    l,
+                    idx,
+                    std::any::type_name::<T>(),
+                    e))
+            .unwrap()
+    })
 }
+
 
 pub fn day_input(day: u8) -> String {
     let path = format!("./days/{}", day);
@@ -60,6 +91,24 @@ macro_rules! day {
 mod tests {
     use super::*;
     use std::panic::catch_unwind;
+
+    #[test]
+    fn str_ext_b_lines_test() {
+        let iter = "foo\nbar\nbaz".b_lines();
+        let v = iter.collect::<Vec<&[u8]>>();
+        assert_eq!(v, &[b"foo", b"bar", b"baz"]);
+        let iter = "foo\nbar\nbaz\n".b_lines();
+        let v = iter.collect::<Vec<&[u8]>>();
+        assert_eq!(v, &[b"foo", b"bar", b"baz"]);
+    }
+
+    #[test]
+    fn str_ext_cut_test() {
+        let res = "foo\nbar".cut('\n');
+        assert_eq!(res, Some(("foo", "bar")));
+        let res = "foo".cut('\n');
+        assert_eq!(res, None);
+    }
 
     #[test]
     fn b_lines_test() {
